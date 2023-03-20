7cc57b3cc23dfefef98524a7cbb633665a22c5d9
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-21 06:55:08 +0700

    import NewsItem Component to News Component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import NewsItem from './NewsItem';
 
 class News extends Component {
 
     render() {
 
         return (
-            <div>News Component</div>
+            <div>
+                News Component 
+                <NewsItem />
+            </div>
         )
     }
 }
